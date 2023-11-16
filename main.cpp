5e3ca513d0a61e9288450a6873129262acdd32f2
zessadqu <zikoessad@gmail.com>
2023-11-16 18:29:17 +0100

    nain

---
+++ main.cpp
@@ -0,0 +1,56 @@
+// irc_server.cpp
+
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main() {
+    // Create a socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        std::cerr << "Error creating socket" << std::endl;
+        return -1;
+    }
+
+    // Bind the socket to a specific IP and port
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(6667); // IRC default port
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+        std::cerr << "Error binding socket" << std::endl;
+        close(serverSocket);
+        return -1;
+    }
+
+    // Listen for incoming connections
+    if (listen(serverSocket, 10) == -1) {
+        std::cerr << "Error listening on socket" << std::endl;
+        close(serverSocket);
+        return -1;
+    }
+
+    std::cout << "IRC Server listening on port 6667..." << std::endl;
+
+    // Accept connections
+    sockaddr_in clientAddr{};
+    socklen_t clientAddrLen = sizeof(clientAddr);
+    int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrLen);
+    if (clientSocket == -1) {
+        std::cerr << "Error accepting connection" << std::endl;
+        close(serverSocket);
+        return -1;
+    }
+
+    std::cout << "Connection accepted from " << inet_ntoa(clientAddr.sin_addr) << std::endl;
+
+    // Handle communication with the client (implement your IRC logic here)
+
+    // Close sockets
+    close(clientSocket);
+    close(serverSocket);
+
+    return 0;
+}
