03f10ba2c569fa3c66ecd09181939a5c47dfb77d
zessadqu <zikoessad@gmail.com>
2023-11-18 08:00:35 +0100

    serv

--- main.cpp
+++ main.cpp
@@ -1,55 +1,103 @@
-// irc_server.cpp
-
 #include <iostream>
 #include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <poll.h>
+#define PORT 6666
+#define MAX_CLIENTS 10
 
 int main() {
+    int ret = 0;
+
     // Create a socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
-        std::cerr << "Error creating socket" << std::endl;
-        return -1;
+        std::cout << "Error in creating the socket" << std::endl;
+        exit(EXIT_FAILURE);
     }
 
     // Bind the socket to a specific IP and port
-    sockaddr_in serverAddr{};
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(6667); // IRC default port
-
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
-        std::cerr << "Error binding socket" << std::endl;
-        close(serverSocket);
-        return -1;
+    sockaddr_in sock_add, client_add;
+    socklen_t client_len = sizeof(client_add);
+    sock_add.sin_family = AF_INET;
+    sock_add.sin_addr.s_addr = INADDR_ANY;
+    sock_add.sin_port = htons(PORT);
+    ret = bind(serverSocket, (sockaddr *)&sock_add, sizeof(sockaddr));
+    if (ret < 0) {
+        std::cout << "Error in binding" << std::endl;
+        exit(EXIT_FAILURE);
     }
 
     // Listen for incoming connections
-    if (listen(serverSocket, 10) == -1) {
-        std::cerr << "Error listening on socket" << std::endl;
-        close(serverSocket);
-        return -1;
+    ret = listen(serverSocket, MAX_CLIENTS);
+    if (ret == -1) {
+        std::cout << "Error while listening" << std::endl;
+        exit(EXIT_FAILURE);
     }
 
-    std::cout << "IRC Server listening on port 6667..." << std::endl;
+    struct pollfd fds[MAX_CLIENTS + 1];
+    int nfds = 1;
+    fds[0].fd = serverSocket;
+    fds[0].events = POLLIN;
 
-    // Accept connections
-    sockaddr_in clientAddr{};
-    socklen_t clientAddrLen = sizeof(clientAddr);
-    int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrLen);
-    if (clientSocket == -1) {
-        std::cerr << "Error accepting connection" << std::endl;
-        close(serverSocket);
-        return -1;
+    int clientSockets[MAX_CLIENTS];
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        clientSockets[i] = -1;  // Initialize with an invalid value
     }
 
-    std::cout << "Connection accepted from " << inet_ntoa(clientAddr.sin_addr) << std::endl;
+    while (1) {
+        ret = poll(fds, nfds, -1);
+        if (ret == -1) {
+            perror("poll");
+            exit(EXIT_FAILURE);
+        }
 
-    // Handle communication with the client (implement your IRC logic here)
+        // Check for incoming connection requests
+        if (fds[0].revents & POLLIN) {
+            int newSocket = accept(serverSocket, (sockaddr *)&client_add, &client_len);
+            if (newSocket == -1) {
+                perror("accept");
+                exit(EXIT_FAILURE);
+            }
 
-    // Close sockets
-    close(clientSocket);
+            int i;
+            for (i = 0; i < MAX_CLIENTS; ++i) {
+                if (clientSockets[i] == -1) {
+                    clientSockets[i] = newSocket;
+                    std::cout << "Client " << i + 1 << " connected from " << inet_ntoa(client_add.sin_addr)
+                              << ":" << ntohs(client_add.sin_port) << std::endl;
+                    break;
+                }
+            }
+
+            if (i == MAX_CLIENTS) {
+                // No available slot for the new client
+                std::cout << "Connection limit reached. Rejecting new connection." << std::endl;
+                close(newSocket);
+            } else {
+                // Add the new client socket to the poll array
+                fds[nfds].fd = newSocket;
+                fds[nfds].events = POLLIN | POLLHUP | POLLERR;
+                nfds++;
+            }
+        }
+
+        // Check for data to read and disconnects on existing client sockets
+        for (int i = 1; i < nfds; ++i) {
+            if (fds[i].revents & (POLLIN | POLLHUP | POLLERR)) {
+                char buffer[1];
+                // Attempt to receive 1 byte from the client
+                int bytesRead = recv(fds[i].fd, buffer, sizeof(buffer), MSG_PEEK);
+                if (bytesRead == 0 || (bytesRead == -1 && (errno == EPIPE || errno == ECONNRESET))) {
+                    // Client has closed the connection
+                    std::cout << "Client " << i << " disconnected." << std::endl;
+                    close(fds[i].fd);
+                    fds[i].fd = -1;
+                }
+            }
+        }
+    }
+
     close(serverSocket);
 
     return 0;
